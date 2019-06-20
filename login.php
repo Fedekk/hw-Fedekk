bb6d95082518e5572da3e617c180e89f3d574f84
Fedek <f.shipwright@gmail.com>
2019-06-20 16:18:26 +0200

    logout completato

--- login.php
+++ login.php
@@ -2,9 +2,10 @@
 include 'Frontend/header.php';
 ?>
 <section>
-    <form action="" method="POST">
+    <form action="#" method="POST">
         <input type="text" placeholder="Nickname" name="nickname">
         <input type="text" placeholder="Password" name="pwd">
+        <label for="ricordami"><input type="checkbox" name="ricordami" id="ricordami">Ricordami</label>
         <input type="submit" value="Accedi">
     </form>
 </section>
